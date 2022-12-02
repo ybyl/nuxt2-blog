fdfb603c42239a17968683a0bbed3a0137d4c1b0
ybyl <49576077+ybyl@users.noreply.github.com>
2022-12-02 17:20:49 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
-  title: 'yun yu yuan', // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: 'yunyuyuan', // github账户名
+  title: 'yuan vercel', // 网站标题
+  nickName: "原vercel博客", // 昵称
+  githubName: 'ybyl', // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
   githubBranch: 'master', // 勿修改
   domain: 'https://blog.halberd.cn', // rss域名
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  SEO_description: '原的vercel博客，书写值得书写的东西',
+  SEO_keywords: 'yuan,ybyl,blog,原vercel,原vercel博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc", // cloudflare的webanalyze
 }
